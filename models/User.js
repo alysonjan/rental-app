8aee954154a1f63654a104a05795b223b04de79b
alysonjan <alysonjan@gmail.com>
2022-04-03 01:25:46 +0800

    refactor: user schema username

--- models/User.js
+++ models/User.js
@@ -2,27 +2,29 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const userSchema = new Schema({
-  lastname: String,
-  firstname: String,
-  email: {
-    type: String,
-    unique: true,
-  },
-  username: {
-    type: String,
-    unique: true,
-  },
-  password: String,
-  roles: {
-    User: {
-      type: Number,
-      default: 2500,
+const userSchema = new Schema(
+  {
+    lastname: String,
+    firstname: String,
+    email: {
+      type: String,
+      unique: true,
     },
-    Editor: Number,
-    Admin: Number,
+    username: String,
+    password: String,
+    roles: {
+      User: {
+        type: Number,
+        default: 2500,
+      },
+      Editor: Number,
+      Admin: Number,
+    },
+    refreshToken: [String],
   },
-  refreshToken: [String],
-})
+  {
+    timestamps: true,
+  }
+)
 
 module.exports = mongoose.model('User', userSchema)
