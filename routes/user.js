cdf047255e4fb3f01373514c11edbbe7fb8a1ce1
alysonjan <alysonjan@gmail.com>
2022-04-04 02:36:46 +0800

    feature: forget pass

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
-const { VALIDATE_SIGN_UP_INPUT, VALIDATE_SIGN_IN_INPUT } = require('../validations/userValidator')
-const { signUp, signIn } = require('../controllers/userController')
+const {
+  VALIDATE_SIGN_UP_INPUT,
+  VALIDATE_SIGN_IN_INPUT,
+  VALIDATE_FORGET_PASSWORD_INPUT,
+} = require('../validations/userValidator')
+const { signUp, signIn, forgetPassword } = require('../controllers/userController')
 
 router.route('/signup').post(VALIDATE_SIGN_UP_INPUT, signUp)
 router.route('/signin').post(VALIDATE_SIGN_IN_INPUT, signIn)
+router.route('/forget-password').post(VALIDATE_FORGET_PASSWORD_INPUT, forgetPassword)
 
 module.exports = router
