d8dab441f3851fd3a2adac011a313452ecce14c1
alysonjan <alysonjan@gmail.com>
2022-04-04 11:27:45 +0800

    feature:alphanumeric

--- constants/regex.js
+++ constants/regex.js
@@ -1,8 +1,7 @@
 // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-
-// const ALPHA_NUMERIC = /^([a-zA-Z0-9 _-]+)$/
-
 const ISALPHA_REGEX = /^[A-Za-z\s]+$/
+// const ALPHA_NUMERIC_WITH_UNDERSCORE = /^([a-zA-Z0-9 _-]+)$/
+// const ALPHA_NUMERIC_ = /^([A-Za-z0-9]+)$/
 
 module.exports = { PASSWORD_REGEX, ISALPHA_REGEX }
