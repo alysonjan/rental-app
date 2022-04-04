b6c2204c98410bee14b404f314acfbae0b80c3b5
alysonjan <alysonjan@gmail.com>
2022-04-04 11:28:53 +0800

    feature: reset password route

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,13 @@ const {
   VALIDATE_SIGN_UP_INPUT,
   VALIDATE_SIGN_IN_INPUT,
   VALIDATE_FORGET_PASSWORD_INPUT,
+  VALIDATE_RESET_PASSWORD_INPUT,
 } = require('../validations/userValidator')
-const { signUp, signIn, forgetPassword } = require('../controllers/userController')
+const { signUp, signIn, forgetPassword, resetPassword } = require('../controllers/userController')
 
 router.route('/signup').post(VALIDATE_SIGN_UP_INPUT, signUp)
 router.route('/signin').post(VALIDATE_SIGN_IN_INPUT, signIn)
 router.route('/forget-password').post(VALIDATE_FORGET_PASSWORD_INPUT, forgetPassword)
+router.route('/reset-password').put(VALIDATE_RESET_PASSWORD_INPUT, resetPassword)
 
 module.exports = router
