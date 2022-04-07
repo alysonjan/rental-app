ac3588c556d1a99ac8ccb6e8e76b6f1cf792a238
alysonjan <alysonjan@gmail.com>
2022-04-08 00:45:26 +0800

    feature: fetch listings

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,6 +1,31 @@
 const Listing = require('../models/Listing')
 const { findByUsername } = require('../controllers/userController')
-const { REQUEST_ERROR_MSG, CREATE_LISTING_SUCESS_MSG } = require('../constants/messages')
+const {
+  REQUEST_ERROR_MSG,
+  CREATE_LISTING_SUCESS_MSG,
+  NO_LISTING_FOUND,
+} = require('../constants/messages')
+
+const fetchListing = async (req, res) => {
+  try {
+    const listings = await Listing.find().sort({ published_at: -1 })
+    if (!listings) return res.status(404).json({ message: NO_LISTING_FOUND })
+    return res.status(200).json(listings)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
+const fetchListingById = async (req, res) => {
+  try {
+    const listingById = await Listing.findById(req.params.id)
+    if (!listingById) return res.status(404).json({ message: NO_LISTING_FOUND })
+    return res.status(200).json(listingById)
+  } catch (err) {
+    if (err.kind === 'ObjectId') return res.status(404).json({ message: NO_LISTING_FOUND })
+    res.status(500).json({ message: err.message })
+  }
+}
 
 const createListing = async (req, res) => {
   const {
@@ -37,8 +62,7 @@ const createListing = async (req, res) => {
     if (newListing) return res.status(201).json({ message: CREATE_LISTING_SUCESS_MSG })
   } catch (err) {
     res.status(500).json({ message: err.message })
-    console.log(err)
   }
 }
 
-module.exports = { createListing }
+module.exports = { createListing, fetchListing, fetchListingById }
