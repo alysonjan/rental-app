617232e513155023aec80d143cba183940634395
alysonjan <alysonjan@gmail.com>
2022-04-08 02:57:40 +0800

    feature: logout

--- routes/user.js
+++ routes/user.js
@@ -7,11 +7,18 @@ const {
   VALIDATE_FORGET_PASSWORD_INPUT,
   VALIDATE_RESET_PASSWORD_INPUT,
 } = require('../validations/userValidator')
-const { signUp, signIn, forgetPassword, resetPassword } = require('../controllers/userController')
+const {
+  signUp,
+  signIn,
+  forgetPassword,
+  resetPassword,
+  logoutHandler,
+} = require('../controllers/userController')
 
 router.route('/signup').post(VALIDATE_SIGN_UP_INPUT, signUp)
 router.route('/signin').post(VALIDATE_SIGN_IN_INPUT, signIn)
 router.route('/forget-password').post(VALIDATE_FORGET_PASSWORD_INPUT, forgetPassword)
 router.route('/reset-password').put(VALIDATE_RESET_PASSWORD_INPUT, resetPassword)
+router.route('/logout').get(logoutHandler)
 
 module.exports = router
